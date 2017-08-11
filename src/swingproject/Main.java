1adcb69815b7525d3bbaed002976e75a014a5ed4
LahiRulZ <lahiru821@gmail.com>
2017-08-11 21:26:16 +0530

    design basic theme

---
+++ src/swingproject/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swingproject;
+
+import GUIs.Theme;
+
+/**
+ *
+ * @author lahir
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Theme window = new Theme();
+        //window.setExtendedState(Theme.MAXIMIZED_BOTH);
+        window.setVisible(true);
+        
+    }
+    
+}
